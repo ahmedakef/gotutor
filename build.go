2a1419b89a5a16d0473b009441898415f80325c7
ahmedakef <aemed.akef.1@gmail.com>
2024-12-12 20:56:54 +0300

    use dlv instead of copying its logic

--- build.go
+++ build.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/go-delve/delve/pkg/gobuild"
+	"github.com/go-delve/delve/pkg/goversion"
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 // given a sourcePath, build the binary in temporary directory and return the path to the binary
-func buildFromFile(sourcePath string, buildFlags string) (string, bool) {
+func buildFromFile(sourcePath string) (string, bool) {
 	tmpDir, err := os.MkdirTemp("", "build")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to create temp directory: %v\n", err)
@@ -23,15 +25,15 @@ func buildFromFile(sourcePath string, buildFlags string) (string, bool) {
 	}
 
 	args := []string{destPath}
-	debugName, success := buildBinary(args, buildFlags, false)
+	debugName, success := buildBinary(args, false)
 	if !success {
 		return "", false
 	}
 	return debugName, true
 }
 
-func buildBinary(args []string, buildFlags string, isTest bool) (string, bool) {
-
+func buildBinary(args []string, isTest bool) (string, bool) {
+	buildFlags := getBuildFlags()
 	var debugName string
 	var err error
 	if isTest {
@@ -72,4 +74,16 @@ func copy(src, dst string) error {
 	}
 
 	return nil
+}
+
+func getBuildFlags() string {
+	buildFlagsDefault := ""
+	if runtime.GOOS == "windows" {
+		ver, _ := goversion.Installed()
+		if ver.Major > 0 && !ver.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 9, Rev: -1}) {
+			// Work-around for https://github.com/golang/go/issues/13154
+			buildFlagsDefault = "-ldflags='-linkmode internal'"
+		}
+	}
+	return buildFlagsDefault
 }
