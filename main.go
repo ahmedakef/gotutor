fd0d59a0529febe2c50917dc9f86d21c14e6f8f6
ahmedakef <aemed.akef.1@gmail.com>
2024-12-12 20:56:54 +0300

    use dlv instead of copying its logic

--- main.go
+++ main.go
@@ -2,37 +2,47 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-delve/delve/pkg/goversion"
-	"github.com/go-delve/delve/service/debugger"
 	"os"
-	"runtime"
-	"sync"
+	"os/signal"
+	"syscall"
 
 	"github.com/go-delve/delve/pkg/gobuild"
 )
 
 func main() {
-	buildFlagsDefault := ""
-	if runtime.GOOS == "windows" {
-		ver, _ := goversion.Installed()
-		if ver.Major > 0 && !ver.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 9, Rev: -1}) {
-			// Work-around for https://github.com/golang/go/issues/13154
-			buildFlagsDefault = "-ldflags='-linkmode internal'"
-		}
-	}
-	debugName, ok := buildFromFile("source", buildFlagsDefault)
+	done := make(chan bool, 1)
+	go waitForSignal(done)
+
+	debugName, ok := buildFromFile("source")
 	if !ok {
 		fmt.Println("Failed to build binary")
 		os.Exit(1)
 	}
 	defer gobuild.Remove(debugName)
-	var targetArgs []string
-	processArgs := append([]string{debugName}, targetArgs...)
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
+	serverDone := make(chan struct{})
 	go func() {
-		execute(0, processArgs, debugger.ExecutingGeneratedFile, debugName, buildFlagsDefault)
-		wg.Done()
+		err := runDebugServer(debugName)
+		if err != nil {
+			fmt.Println("Failed to run debug server", err)
+			os.Exit(1)
+		}
+		close(serverDone)
 	}()
-	wg.Wait()
+
+	select {
+	case <-done:
+		fmt.Println("Exiting")
+	case <-serverDone:
+		fmt.Println("Exiting")
+	}
+}
+
+func waitForSignal(done chan bool) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-sigs
+	fmt.Println("\nreceived:", sig)
+	done <- true
+
 }
