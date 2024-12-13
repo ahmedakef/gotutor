d0226adbe9796c9b2f864129244101253c0ca349
ahmedakef <aemed.akef.1@gmail.com>
2024-12-13 16:35:55 +0300

    get user gorountines

--- serialize/visualize.go
+++ serialize/visualize.go
@@ -3,6 +3,7 @@ package serialize
 import (
 	"github.com/go-delve/delve/service/api"
 	"github.com/go-delve/delve/service/rpc2"
+	"strings"
 )
 
 type serializer struct {
@@ -21,7 +22,7 @@ func (v *serializer) ExecutionSteps() ([]Step, error) {
 	debugState := <-v.client.Continue()
 	for !debugState.Exited {
 		if len(goroutines) == 0 {
-			goroutines, _, err = v.client.ListGoroutines(0, 0)
+			goroutines, err = v.getUserGoroutines()
 			if err != nil {
 				return steps, err
 			}
@@ -57,4 +58,18 @@ func (v *serializer) initMainBreakPoint() {
 		Name:         "main",
 		FunctionName: "main.main",
 	})
+}
+
+func (v *serializer) getUserGoroutines() ([]*api.Goroutine, error) {
+	goroutines, _, err := v.client.ListGoroutines(0, 0)
+	if err != nil {
+		return nil, err
+	}
+	var filteredGoroutines []*api.Goroutine
+	for _, goroutine := range goroutines {
+		if !strings.Contains(goroutine.CurrentLoc.File, "src/runtime/") {
+			filteredGoroutines = append(filteredGoroutines, goroutine)
+		}
+	}
+	return filteredGoroutines, nil
 }
