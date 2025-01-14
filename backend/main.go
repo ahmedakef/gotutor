e2604995f40d3628e9215e359a6bf80790518eb9
ahmedakef <aemed.akef.1@gmail.com>
2025-01-14 10:12:03 +0100

    no need to custom server, the default server already handle cors

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"context"
 	"os"
 	"time"
 
@@ -16,20 +15,10 @@ func main() {
 		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
 	).Level(zerolog.TraceLevel).With().Timestamp().Caller().Logger()
 
-	rs, err := server.NewRestate().
-		Bind(restate.Reflect(newHandler(logger))).
-		Bidirectional(false).
-		Handler()
-	if err != nil {
-		logger.Fatal().Err(err).Msg("failed to create Restate server")
+	rs := server.NewRestate().
+		Bind(restate.Reflect(newHandler(logger)))
+
+	if err := rs.Start(context.Background(), ":9080"); err != nil {
+		logger.Fatal().Err(err).Msg("failed to start HTTP server")
 	}
-
-	handler := corsMiddleware(rs)
-
-	port := ":9080"
-	logger.Info().Msg(fmt.Sprintf("Server is running on %s...", port))
-	if err := http.ListenAndServe(port, handler); err != nil {
-		logger.Fatal().Err(err).Msg("failed to start HTTP/2 server")
-	}
-
 }
