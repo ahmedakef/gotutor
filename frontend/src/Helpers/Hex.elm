fe91d716add54b4de66e57400a163e87a8a5802a
ahmedakef <aemed.akef.1@gmail.com>
2025-01-19 17:25:13 +0100

    refactor: comment unused hex conversion functions

--- frontend/src/Helpers/Hex.elm
+++ frontend/src/Helpers/Hex.elm
@@ -22,75 +22,52 @@ intToHex n =
 
 
 
--- not currently used
-
-
-toHexHelper : Int -> String
-toHexHelper n =
-    if n < 16 then
-        singleHexDigit n
-
-    else
-        let
-            quotient =
-                n // 16
-
-            remainder =
-                modBy 16 n
-        in
-        toHexHelper quotient ++ singleHexDigit remainder
-
-
-singleHexDigit : Int -> String
-singleHexDigit n =
-    case n of
-        0 ->
-            "0"
-
-        1 ->
-            "1"
-
-        2 ->
-            "2"
-
-        3 ->
-            "3"
-
-        4 ->
-            "4"
-
-        5 ->
-            "5"
-
-        6 ->
-            "6"
-
-        7 ->
-            "7"
-
-        8 ->
-            "8"
-
-        9 ->
-            "9"
-
-        10 ->
-            "a"
-
-        11 ->
-            "b"
-
-        12 ->
-            "c"
-
-        13 ->
-            "d"
-
-        14 ->
-            "e"
-
-        15 ->
-            "f"
-
-        _ ->
-            ""
+-- toHexHelper : Int -> String
+-- toHexHelper n =
+--     if n < 16 then
+--         singleHexDigit n
+--     else
+--         let
+--             quotient =
+--                 n // 16
+--             remainder =
+--                 modBy 16 n
+--         in
+--         toHexHelper quotient ++ singleHexDigit remainder
+-- singleHexDigit : Int -> String
+-- singleHexDigit n =
+--     case n of
+--         0 ->
+--             "0"
+--         1 ->
+--             "1"
+--         2 ->
+--             "2"
+--         3 ->
+--             "3"
+--         4 ->
+--             "4"
+--         5 ->
+--             "5"
+--         6 ->
+--             "6"
+--         7 ->
+--             "7"
+--         8 ->
+--             "8"
+--         9 ->
+--             "9"
+--         10 ->
+--             "a"
+--         11 ->
+--             "b"
+--         12 ->
+--             "c"
+--         13 ->
+--             "d"
+--         14 ->
+--             "e"
+--         15 ->
+--             "f"
+--         _ ->
+--             ""
