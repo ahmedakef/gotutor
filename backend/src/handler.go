4eba1b80a05d3f7d8e7eedf5b3933985736cc822
ahmedakef <aemed.akef.1@gmail.com>
2025-02-23 15:06:24 +0100

    move backend code into src/ dir

---
+++ backend/src/handler.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/ahmedakef/gotutor/backend/src/cache"
+	"github.com/ahmedakef/gotutor/serialize"
+	"github.com/rs/zerolog"
+	"golang.org/x/exp/rand"
+	"golang.org/x/sync/semaphore"
+)
+
+const _allowedConcurrency = 10
+
+// Handler is a struct which represents the backend handler
+type Handler struct {
+	logger zerolog.Logger
+	cache  cache.LRUCache
+}
+
+func newHandler(logger zerolog.Logger, cache cache.LRUCache) *Handler {
+	return &Handler{
+		logger: logger,
+		cache:  cache,
+	}
+}
+
+type GetExecutionStepsRequest struct {
+	SourceCode string `json:"source_code"`
+}
+
+func (h *Handler) GetExecutionSteps(ctx context.Context, req GetExecutionStepsRequest) (serialize.ExecutionResponse, error) {
+	// check if the request is already in the cache
+	cachedResponse, ok := h.cache.Get(req.SourceCode)
+	if ok {
+		return cachedResponse, nil
+	}
+
+	sem := semaphore.NewWeighted(_allowedConcurrency)
+	if err := sem.Acquire(ctx, 1); err != nil {
+		return serialize.ExecutionResponse{}, fmt.Errorf("failed to acquire semaphore: %w", err)
+	}
+	defer sem.Release(1)
+
+	port := generateRandomPort()
+	dataDir, err := prepareTempDir(port)
+	if err != nil {
+		return serialize.ExecutionResponse{}, fmt.Errorf("failed to prepare sources directory: %w", err)
+	}
+	defer func() {
+		err := os.RemoveAll(dataDir)
+		if err != nil {
+			h.logger.Error().Err(err).Msg("failed to remove sources directory")
+		}
+	}()
+	sourcePath := fmt.Sprintf("%s/main.go", dataDir)
+	err = writeSourceCodeToFile(sourcePath, req.SourceCode)
+	if err != nil {
+		return serialize.ExecutionResponse{}, fmt.Errorf("failed to write source code to file: %w", err)
+	}
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return serialize.ExecutionResponse{}, fmt.Errorf("failed to get current directory: %w", err)
+	}
+	sourceCodeMapping := fmt.Sprintf("%s/%s:/data/main.go", currentDir, sourcePath)
+	outputMapping := fmt.Sprintf("%s/%s/output:/root/output", currentDir, dataDir)
+	deadlineCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	dockerCommand := exec.CommandContext(deadlineCtx, "docker", "run", "--rm", "-v", sourceCodeMapping, "-v", outputMapping, "ahmedakef/gotutor", "debug", "/data/main.go")
+	out, err := dockerCommand.CombinedOutput()
+	if err != nil || outputContainsError(string(out)) {
+		if deadlineCtx.Err() == context.DeadlineExceeded {
+			return serialize.ExecutionResponse{}, fmt.Errorf("execution timed out, remove infinte loops or long waiting times")
+		}
+		return serialize.ExecutionResponse{}, fmt.Errorf("failed to run docker command: %w : %s", err, string(out))
+	}
+
+	output, err := readFileToString(fmt.Sprintf("%s/output/steps.json", dataDir))
+	if err != nil {
+		return serialize.ExecutionResponse{}, fmt.Errorf("failed to read output file: %w", err)
+	}
+	// decode the output
+	var response serialize.ExecutionResponse
+	err = json.NewDecoder(strings.NewReader(output)).Decode(&response)
+	if err != nil {
+		return serialize.ExecutionResponse{}, fmt.Errorf("failed to decode output: %w", err)
+	}
+	response.Output = string(out)
+	h.cache.Set(req.SourceCode, response)
+	return response, nil
+}
+
+func generateRandomPort() int {
+	rand.Seed(uint64(time.Now().UnixNano()))
+	return rand.Intn(65535-1024) + 1024
+}
+
+func prepareTempDir(randomPort int) (string, error) {
+	dir := fmt.Sprintf("data/%d", randomPort)
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return "", fmt.Errorf("create sources directory: %w", err)
+	}
+	return dir, nil
+}
+
+func writeSourceCodeToFile(sourcePath, sourceCode string) error {
+	file, err := os.OpenFile(sourcePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return fmt.Errorf("create %s file: %w", sourcePath, err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(sourceCode)
+	if err != nil {
+		return fmt.Errorf("write to %s file: %w", sourcePath, err)
+	}
+	return nil
+}
+
+func readFileToString(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", fmt.Errorf("open %s file: %w", filePath, err)
+	}
+	defer file.Close()
+
+	contents, err := io.ReadAll(file)
+	if err != nil {
+		return "", fmt.Errorf("read %s file: %w", filePath, err)
+	}
+	return string(contents), nil
+}
+
+func outputContainsError(output string) bool {
+	return strings.Contains(output, "failed to build binary")
+}
---
+++ backend/src/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/ahmedakef/gotutor/backend/src/cache"
+	"github.com/rs/zerolog"
+)
+
+const (
+	_port          = 8080
+	_maxCacheSize  = 100 * 1024 * 1024 // 100MB
+	_maxCacheItems = 100
+	_cacheTTL      = 0
+)
+
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func respondWithError(w http.ResponseWriter, message string, statusCode int) {
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(ErrorResponse{Message: message})
+}
+
+func respondWithJSON(w http.ResponseWriter, data interface{}, statusCode int) error {
+	w.WriteHeader(statusCode)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func main() {
+	logger := zerolog.New(
+		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
+	).Level(zerolog.InfoLevel).With().Timestamp().Caller().Logger()
+
+	cache := cache.NewLRUCache(_maxCacheSize, _maxCacheItems, _cacheTTL)
+
+	handler := newHandler(logger, cache)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/GetExecutionSteps", func(w http.ResponseWriter, r *http.Request) {
+		var req GetExecutionStepsRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			respondWithError(w, "failed to decode request", http.StatusBadRequest)
+			return
+		}
+
+		resp, err := handler.GetExecutionSteps(r.Context(), req)
+		if err != nil {
+			respondWithError(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = respondWithJSON(w, resp, http.StatusOK)
+		if err != nil {
+			logger.Error().Err(err).Msg("failed to respond with JSON")
+		}
+	})
+
+	logger.Info().Msg(fmt.Sprintf("starting server on http://localhost:%d", _port))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", _port), corsMiddleware(mux)); err != nil {
+		logger.Fatal().Err(err).Msg("failed to start server")
+	}
+
+}
